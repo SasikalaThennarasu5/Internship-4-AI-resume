448f130562080d6d3ec4a7c73cd021470acc738e
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-03-24 16:09:54 +0530

    add

--- frontend/src/components/home/StatsBar.jsx
+++ frontend/src/components/home/StatsBar.jsx
@@ -2,16 +2,23 @@ export default function StatsBar() {
   return (
     <div className="overflow-hidden bg-purple-500 text-white py-3 text-sm">
 
-      <div className="whitespace-nowrap flex gap-10 animate-marquee">
+      <div className="whitespace-nowrap flex gap-20 animate-marquee">
         {/* Duplicate content for smooth loop */}
-        <div className="flex gap-10">
+        <div className="flex gap-20">
           <p>⭐ ATS-friendly template</p>
           <p>⭐ AI writing suggestions</p>
           <p>⭐ Instant download</p>
           <p>⭐ ATS-friendly template</p>
         </div>
 
-        <div className="flex gap-10">
+        <div className="flex gap-20">
+          <p>⭐ ATS-friendly template</p>
+          <p>⭐ AI writing suggestions</p>
+          <p>⭐ Instant download</p>
+          <p>⭐ ATS-friendly template</p>
+        </div>
+
+        <div className="flex gap-20">
           <p>⭐ ATS-friendly template</p>
           <p>⭐ AI writing suggestions</p>
           <p>⭐ Instant download</p>
