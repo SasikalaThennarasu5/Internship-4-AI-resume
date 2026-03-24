83fd63831e28176bf0433f630ac3c5b61a35e174
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-03-24 11:13:13 +0530

    add

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend is running 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('resume.urls')),
     path('api/auth/', include('accounts.urls')),
